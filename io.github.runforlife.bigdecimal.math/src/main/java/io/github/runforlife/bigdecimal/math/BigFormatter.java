42daab0b842cce83b142e69f3d0abc3dd339bfc6
Oleg Yushko <cska.kiev.ua@gmail.com>
2025-01-08 23:39:06 +0200

    Implementing BigFormatter.java

--- io.github.runforlife.bigdecimal.math/src/main/java/io/github/runforlife/bigdecimal/math/BigFormatter.java
+++ io.github.runforlife.bigdecimal.math/src/main/java/io/github/runforlife/bigdecimal/math/BigFormatter.java
@@ -148,8 +148,8 @@ public class BigFormatter {
 
         if (valueWithoutTrailingZeros.compareTo(BigDecimal.ZERO) > 0) {
             if (scale > 0) {
-                if (scale > precision) {
-                    if (DOT_SYMBOL_LENGTH + scale + precision <= numberOfCharacters) {
+                if (precision > 1) {
+                    if (scale + precision <= numberOfCharacters) {
                         return String.valueOf(valueWithoutTrailingZeros);
                     } else {
                         int scaleNumberOfDigits = IntLongUtils.numberOfDigits(scale);
@@ -169,13 +169,25 @@ public class BigFormatter {
                         return formattedValue;
                     }
                 } else {
-                    if (precision + scale <= numberOfCharacters) {
-//                    BigDecimal valueFormatted = valueWithoutTrailingZeros.setScale(0, roundingMode);
-//                    return String.valueOf(valueFormatted);
+                    if (DOT_SYMBOL_LENGTH + precision + scale <= numberOfCharacters) {
                         return String.valueOf(valueWithoutTrailingZeros);
-                    }
+                    } else {
+                        int scaleNumberOfDigits = IntLongUtils.numberOfDigits(scale);
 
-                    return String.valueOf(valueWithoutTrailingZeros);
+                        StringBuilder pattern = new StringBuilder("0.");
+                        for (int i = 0; i < numberOfCharacters - DOT_SYMBOL_LENGTH - SIGN_SYMBOL_LENGTH - EXPONENT_SYMBOL_LENGTH - scaleNumberOfDigits; i++) {
+                            pattern.append("#");
+                        }
+                        pattern.append("E0");
+                        String patternString = pattern.toString();
+
+                        DecimalFormat formatter = new DecimalFormat(patternString, DecimalFormatSymbols.getInstance(Locale.ENGLISH));
+                        formatter.setRoundingMode(roundingMode);
+                        formatter.setMinimumFractionDigits(0);
+                        String formattedValue = formatter.format(value);
+
+                        return formattedValue;
+                    }
                 }
 //                StringBuilder pattern = new StringBuilder("0.");
 //                for (int i = 0; i < numberOfCharacters - 2; i++) {
